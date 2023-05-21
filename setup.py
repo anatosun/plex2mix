55be5341543b3acef170ce76da88ed859bdcf28f
anatosun <z4jyol8l@duck.com>
2023-05-21 17:14:01 +0200

    build: fixed setup.py install

--- setup.py
+++ setup.py
@@ -4,6 +4,10 @@ setup(
     name='plex2mix',
     version='0.1.0',
     packages=find_packages(),
+    description='Python cli utility to download Plex playlists.',
+    author='Anatosun',
+    author_email='z4jyol8l@duck.com',
+    url='https://github.com/anatosun/plex2mix',
     include_package_data=True,
     install_requires=[
         'Click',
