61cf712df6b0f83dbaf2228d31cdbdca364663e1
François-Xavier Wicht <francois-xavier.wicht@unibe.ch>
2023-09-24 17:16:19 +0200

    refactor: streamlined download logic

--- plex2mix/downloader.py
+++ plex2mix/downloader.py
@@ -1,10 +1,10 @@
 from concurrent.futures import ThreadPoolExecutor
 import os
-import re
 import unicodedata
 from plexapi.server import PlexServer
 from plexapi.playlist import Playlist
 from plexapi.audio import Track
+from plex2mix.exporter import Exporter
 
 
 class Downloader:
@@ -15,7 +15,8 @@ class Downloader:
         self.path = os.path.expanduser(os.path.join(path))
         self.playlists_path = os.path.expanduser(os.path.join(playlists_path))
         self.pool = ThreadPoolExecutor(max_workers=threads)
-        self.downloadedTracks = []
+        self.exporter = Exporter(self.playlists_path)
+        self.downloaded = []
 
     def get_playlists(self) -> list:
         if self.playlists is None:
@@ -28,54 +29,44 @@ class Downloader:
         return [p.title for p in self.playlists]
 
     def __download_track(self, track: Track,  overwrite=False) -> str:
-        album_path, filepath = self.get_path(track)
+        artist, album = track.grandparentTitle, track.parentTitle
+        album_path = os.path.join(
+            self.path, self.__normalize_path(artist), self.__normalize_path(album))
+        _, file = os.path.split(track.media[0].parts[0].file)
+        file = self.__normalize_path(file)
+        filepath = os.path.join(album_path, file)
         size_on_server = track.media[0].parts[0].size
-        if os.path.exists(filepath):
+
+        if not hasattr(track, 'filepath'):
+            track.filepath = filepath
+        if not hasattr(track, 'album_path'):
+            track.album_path = album_path
+
+        if os.path.exists(os.path.join(filepath)):
             while overwrite or size_on_server > os.path.getsize(filepath):
                 track.download(album_path, keep_original_name=True)
                 overwrite = False
         else:
             track.download(album_path, keep_original_name=True)
 
-        self.downloadedTracks.append(filepath)
+        self.downloaded.append(filepath)
 
         return filepath
-
-    def get_path(self, track: Track) -> tuple:
-        artist, album = track.grandparentTitle, track.parentTitle
-        album_path = os.path.join(
-            self.path, self.__normalize(artist), self.__normalize(album))
-        _, file = os.path.split(self.__normalize(track.media[0].parts[0].file))
-        filepath = os.path.join(album_path, file)
-        return album_path, filepath
-
-    def dump_m3u8(self, playlist: Playlist) -> None:
-        title = playlist.title.strip()
-        path = os.path.join(self.playlists_path,
-                            f"{title}.m3u8")
-        f = open(path, "w", encoding="utf-8")
-        f.write("#EXTM3u\n")
-        for track in playlist.items():
-            if track.duration and track.grandparentTitle and track.parentTitle is not None:
-                _, filepath = self.get_path(track)
-                m3u8 = f"#EXTINF:{track.duration // 1000},{track.grandparentTitle} - {track.title}\n#EXT-X-RATING:{track.userRating if track.userRating is not None else 0}\n{filepath}\n"
-                f.write(m3u8)
 
     def futures(self):
         return self.futures
 
-    def download(self, playlist: Playlist, overwrite=False) -> list:
-
+    def download(self, playlist: Playlist, overwrite=False, dump_m3u8=True, dump_itunes=False) -> list:
         tasks = []
         for track in playlist.items():
             future = self.pool.submit(self.__download_track, track, overwrite)
             tasks.append(future)
-        self.pool.submit(self.dump_m3u8, playlist)
+        self.pool.submit(self.exporter.proceed, playlist,
+                         dump_m3u8, dump_itunes)
         return tasks
 
-    def __normalize(self, path: str):
+    def __normalize_path(self, path: str):
         path = unicodedata.normalize('NFKC', path)
         path = unicodedata.normalize('NFKD', path).encode(
             'ascii', 'ignore').decode('ascii')
-        path = re.sub(r'[^\w\s-]', '', path)
         return path
